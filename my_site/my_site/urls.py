dfdd86061730ae5ea912c29f8c7aab0277c53b83
Yangzhedi <uiryzd@163.com>
2019-03-21 20:52:49 +0800

    feat(restframework): add Django restframework

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
+
+# from blog.apis import post_list, PostList
+from blog.apis import PostViewSet
+
+router = DefaultRouter()
+router.register(r'post', PostViewSet, base_name='api-post')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # url(r'^api/post/', PostList.as_view(), name='post-list'),
+    url(r'^api/', include(router.urls, namespace="api")),
+    url(r'^api/docs/', include_docs_urls(title='site apis')),
 ]
+# http://127.0.0.1:8088/api/post/?format=json
