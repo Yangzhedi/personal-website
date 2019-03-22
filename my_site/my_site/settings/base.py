a976b7a21415d5da856284516f42f372e396bdcb
Yangzhedi <uiryzd@163.com>
2019-03-22 17:46:23 +0800

    feat(settings): add pagination & mysql

--- my_site/my_site/settings/base.py
+++ my_site/my_site/settings/base.py
@@ -126,6 +126,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- my_site/my_site/settings/develop.py
+++ my_site/my_site/settings/develop.py
@@ -1,10 +1,24 @@
 from .base import * # NOQA
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'site_db',
+        'USER': 'root',
+        'PASSWORD': 'PASSWORD',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
