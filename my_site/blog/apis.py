74c76e37b7b77498c4865600bc3f881b25917a64
Yangzhedi <uiryzd@163.com>
2019-05-10 10:47:20 +0800

    fix(api): fix api and urls

--- my_site/blog/apis.py
+++ my_site/blog/apis.py
@@ -1,7 +1,11 @@
-from rest_framework import generics, viewsets
+import datetime
+
+from rest_framework import generics, viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.decorators import detail_route
+from django.shortcuts import get_object_or_404
 
 from .models import Post
 from .serializers import PostSerializer, PostDetailSerializer
@@ -22,8 +26,101 @@ from .serializers import PostSerializer, PostDetailSerializer
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
+    permission_classes_by_action = {
+        'create': [IsAdminUser],
+        'list': [AllowAny],
+        'retrieve': [AllowAny]
+    }
 
+    # read
+    # GET /api/post/{id}/
     def retrieve(self, request, *args, **kwargs):
         self.serializer_class = PostDetailSerializer
         return super().retrieve(request, *args, **kwargs)
+
+    # create
+    # POST /api/post/
+    def create(self, request, *args, **kwargs):
+        return super(PostViewSet, self).create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        return super(PostViewSet, self).list(request, *args, **kwargs)
+
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action`
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+
+
+class PostLatestViewSet(viewsets.ModelViewSet):
+    serializer_class = PostDetailSerializer
+    permission_classes = [AllowAny]
+    def latest(self, request):
+        post = Post.objects.latest('id')
+        serializer = self.get_serializer(post)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def post_test_list(request):
+    permission_classes = [AllowAny]
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        # print(request.data)
+        # serializer = PostSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        post = Post.objects.latest('id')
+        # print(post)
+        serializer = PostDetailSerializer(post)
+        # serializer = self.get_serializer(post)
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def get_post_by_url(request):
+    permission_classes = [AllowAny]
+    """
+    List all code snippets, or create a new snippet.
+    """
+    req = request.data
+    year = request.data['year']
+    month = request.data['month']
+    print(req['year'], req['month'])
+    # post = Post.objects.latest('id')
+    # post = Post.objects.filter(create_time__year=req['year'])[:1].get()
+    # postQuerySet = Post.objects.filter(create_time__year=req['year']) \
+    #     .filter(create_time__month=req['month']) \
+    #     .filter(url=req['url'])
+
+    day_max = 30
+    months = [1, 3, 5, 7, 8, 10, 12]
+    if int(month) in months:
+        day_max = 31
+
+    post_query_set = Post.objects.filter(
+        create_time__range=(datetime.date(int(year), int(month), 1), datetime.date(int(year), int(month), day_max))
+    ).filter(url=req['url'])
+
+    print(len(post_query_set))
+
+    # post = Post.objects.get(create_time__year=req['year'], create_time__month=req['month'])
+
+    post = get_object_or_404(post_query_set)
+    # print(post)
+    serializer = PostDetailSerializer(post)
+    # serializer = self.get_serializer(post)
+    return Response(serializer.data)
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -19,16 +19,23 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.documentation import include_docs_urls
 
 # from blog.apis import post_list, PostList
-from blog.apis import PostViewSet
+from blog.apis import PostViewSet, PostLatestViewSet, post_test_list, get_post_by_url
 
 router = DefaultRouter()
 router.register(r'post', PostViewSet, base_name='api-post')
+print(router)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # url(r'^api/post/', PostList.as_view(), name='post-list'),
     url(r'^api/', include(router.urls, namespace="api")),
+
+    url(r'^api/post/latest', PostLatestViewSet.as_view({'post': 'latest'})),
+    url(r'^api/post/v1/list', post_test_list),
+    url(r'^api/post/get_by_url', get_post_by_url),
+
     url(r'^api/docs/', include_docs_urls(title='site apis')),
+
 ]
 # http://127.0.0.1:8088/api/post/?format=json
